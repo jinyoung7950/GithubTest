e73e7f2136065f65352ee055967bee069250033c
jinyoung <jinyoung@lectopia.com>
2016-06-18 15:08:45 +0900

    Web Test Success

---
+++ GithubTest/src/com/lectopia/git/GitTestServlet.java
@@ -0,0 +1,27 @@
+package com.lectopia.git;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GitTestServlet
+ */
+public class GitTestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+  
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("result.jsp").forward(request, response);
+	}
+
+}
